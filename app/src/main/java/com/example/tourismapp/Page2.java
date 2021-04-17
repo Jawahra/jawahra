74c4ddc052acaeeeea7001b87b8986740fe5e34a
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-04-17 14:45:50 +0400

    test

--- app/src/main/java/com/example/tourismapp/Page2.java
+++ app/src/main/java/com/example/tourismapp/Page2.java
@@ -1,24 +1,25 @@
 package com.example.tourismapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class Page2 extends AppCompatActivity {
     private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.page2);
 
-        button=findViewById(R.id.btn);
+        button=findViewById(R.id.back_btn);
         button.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, Page2.class);
+            Intent i = new Intent(Page2.this, MainActivity.class);
             startActivity(i);
         });
+
     }
 }
