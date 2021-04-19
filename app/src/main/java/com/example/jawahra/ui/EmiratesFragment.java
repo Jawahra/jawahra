fc2ff4e6b8f4c6cf729b4b183e37bf5c5429c653
jayceebuarao <80630074+jayceebuarao@users.noreply.github.com>
2021-04-19 23:20:00 +0400

    Added Necessary Fragments

---
+++ app/src/main/java/com/example/jawahra/ui/EmiratesFragment.java
@@ -0,0 +1,66 @@
+package com.example.jawahra.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jawahra.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EmiratesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EmiratesFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public EmiratesFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EmiratesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EmiratesFragment newInstance(String param1, String param2) {
+        EmiratesFragment fragment = new EmiratesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_emirates, container, false);
+    }
+}
