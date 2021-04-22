5134fa9ab3ac894d8607dfd45d869324d68edeb9
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-04-22 22:38:12 +0400

    Added user authentication (#4)
    * Added user authentication
    
    * Fixed sign up bug
    
    * Changed google-services.json file
    
    * Added Google Sign-In
    
    * Added Facebook sign-in
    
    * Fixed Facebook sign-in
    
    * Updated user authentication

--- app/src/main/java/com/example/jawahra/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/jawahra/ui/profile/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.jawahra.ui.profile;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,29 +13,93 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.jawahra.AuthActivity;
+import com.example.jawahra.LoginActivity;
 import com.example.jawahra.R;
+import com.example.jawahra.User;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
+    private FirebaseUser user;
+
+    private DatabaseReference reference;
+    private String userID;
+    private GoogleSignInClient gsi;
+
+    private Button btnLogout;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-//        {return inflater.inflate(R.layout.fragment_profile, container, false);
-        profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_events, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView usernameText = view.findViewById(R.id.profile_name);
+        final TextView emailText = view.findViewById(R.id.profile_email);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Initialize sign in client
+        gsi = GoogleSignIn.getClient(getActivity(), gso);
+
+        btnLogout = view.findViewById(R.id.btn_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut(); // Log out from email
+                gsi.signOut(); // Log out from Google
+                LoginManager.getInstance().logOut(); // Log out from Facebook
+
+                // Redirect to Login Activity
+                startActivity(new Intent(getActivity(), LoginActivity.class));
             }
         });
-        return root;
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String username = userProfile.username;
+                    String email = userProfile.email;
+
+                    usernameText.setText(username);
+                    emailText.setText(email);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "There has been an error!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
