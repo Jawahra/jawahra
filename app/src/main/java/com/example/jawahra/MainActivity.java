7af4ca2c7106e56bcc84eb50f4d1ab81e8dcbf20
Anna Montero <71811524+AnnaKMontero@users.noreply.github.com>
2021-04-22 22:29:23 +0400

    Remove action bar

--- app/src/main/java/com/example/jawahra/MainActivity.java
+++ app/src/main/java/com/example/jawahra/MainActivity.java
@@ -19,11 +19,14 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_visit, R.id.navigation_events, R.id.navigation_profile)
-                .build();
+
+
+//        Code for app bar not needed but i didn't know what to do with it so i comment out
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_visit, R.id.navigation_events, R.id.navigation_profile)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 }
