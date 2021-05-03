2c5765694d01dcebc658ae74693a0e371a99dac1
Anna Montero <71811524+AnnaKMontero@users.noreply.github.com>
2021-05-03 12:56:23 +0400

    Create place and event details fragment

--- app/src/main/java/com/example/jawahra/ui/PlaceDetailsFragment.java
+++ app/src/main/java/com/example/jawahra/ui/PlaceDetailsFragment.java
@@ -1,16 +1,15 @@
 package com.example.jawahra.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.jawahra.R;
 
-public class EventDetailsFragment extends Fragment {
+public class PlaceDetailsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
