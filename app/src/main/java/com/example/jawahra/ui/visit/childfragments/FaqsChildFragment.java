5e5b060d2886874ad3ab6baa213894848a7f923d
jayceebuarao <80630074+jayceebuarao@users.noreply.github.com>
2021-05-18 21:18:08 +0400

    construct xml

--- app/src/main/java/com/example/jawahra/ui/visit/childfragments/FaqsChildFragment.java
+++ app/src/main/java/com/example/jawahra/ui/visit/childfragments/FaqsChildFragment.java
@@ -19,6 +19,8 @@ public class FaqsChildFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faqs_child, container, false);
+        View root = inflater.inflate(R.layout.fragment_faqs_child, container, false);
+
+        return root;
     }
 }
