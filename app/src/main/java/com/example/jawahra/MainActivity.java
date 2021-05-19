ec09d70f64727f84ff917c6fa15492039af3a3f8
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-05-19 18:10:44 +0400

    Save user location to firestore

--- app/src/main/java/com/example/jawahra/MainActivity.java
+++ app/src/main/java/com/example/jawahra/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.jawahra;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.jawahra.models.UserLocationModel;
+import com.example.jawahra.models.UserModel;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 
 
 import androidx.annotation.NonNull;
@@ -33,7 +47,10 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 702;
     private static final int ERROR_DIALOG_REQUEST = 703;
 
+    private FirebaseFirestore fStore;
     private boolean locationPermissionGranted = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private UserLocationModel userLocationModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +67,76 @@ public class MainActivity extends AppCompatActivity {
 //      NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         NavigationUI.setupWithNavController(navView, navController);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fStore = FirebaseFirestore.getInstance();
+
+    }
+
+    private void getUserDetails() {
+        if (userLocationModel == null) {
+            userLocationModel = new UserLocationModel();
+
+            DocumentReference userRef = fStore.collection("users")
+                    .document(FirebaseAuth.getInstance().getUid());
+
+            userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()){
+                        Log.d("CHECK_MAP", "OnComplete: successfully saved user details");
+                        UserModel userModel = task.getResult().toObject(UserModel.class);
+
+                        userLocationModel.setUserModel(userModel);
+                        getLastKnownLocation();
+                    }
+                }
+            });
+        } else {
+            getLastKnownLocation();
+        }
+    }
+
+    private void getLastKnownLocation() {
+        Log.d("CHECK_MAP", "getLastKnownLocation: called.");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+                    userLocationModel.setGeoPoint(geoPoint);
+                    userLocationModel.setTimestamp(null);
+                    saveUserLocation();
+                }
+            }
+        });
+
+    }
+
+    private void saveUserLocation(){
+
+        if(userLocationModel != null){
+            DocumentReference locationRef = fStore
+                    .collection("user collection")
+                    .document(FirebaseAuth.getInstance().getUid());
+
+            locationRef.set(userLocationModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Log.d("CHECK_MAP", "saveUserLocation: \ninserted user location into database." +
+                                "\n latitude: " + userLocationModel.getGeoPoint().getLatitude() +
+                                "\n longitude: " + userLocationModel.getGeoPoint().getLongitude());
+                    }
+                }
+            });
+        }
     }
 
     private boolean checkMapServices(){
@@ -95,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             locationPermissionGranted = true;
+            getUserDetails();
         } else {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
@@ -144,6 +232,7 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case PERMISSIONS_REQUEST_ENABLE_GPS: {
                 if(locationPermissionGranted){
+                    getUserDetails();
                 }
                 else{
                     getLocationPermission();
@@ -158,7 +247,9 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         if(checkMapServices()){
             if(locationPermissionGranted){
-
+                if(FirebaseAuth.getInstance().getCurrentUser() != null ){
+                    getUserDetails();
+                }
             }
             else{
                 getLocationPermission();
