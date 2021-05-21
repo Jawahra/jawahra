b98d842cfaaa645f6a17cf33d8138a164ec20686
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-05-22 00:42:54 +0400

    Add location permissions

--- app/src/main/java/com/example/jawahra/ui/MapsFragment.java
+++ app/src/main/java/com/example/jawahra/ui/MapsFragment.java
@@ -1,100 +1,89 @@
 package com.example.jawahra.ui;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.jawahra.MainActivity;
 import com.example.jawahra.R;
-import com.example.jawahra.models.PlaceDetailsModel;
-import com.example.jawahra.models.PlacesModel;
 import com.example.jawahra.ui.visit.PlaceDetailsFragment;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
+    // constants
+    private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 701;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 702;
+    private static final int ERROR_DIALOG_REQUEST = 703;
+
+    private boolean locationPermissionGranted = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    // database
     private FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     private CollectionReference detailsRef;
     public String emirateId, placeId, placeTitle;
-
-    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-
-            //get bundle values
-            Bundle bundle = getArguments();
-            if(bundle != null){
-                emirateId = bundle.getString("emirateId");
-                placeId = bundle.getString("placeId");
-                placeTitle = bundle.getString("placeName");
-
-            }
-
-            detailsRef = fStore.collection("emirates")
-                    .document(emirateId)
-                    .collection("places")
-                    .document(placeId)
-                    .collection("details");
-
-            detailsRef.get()
-                    .addOnSuccessListener(snapshot -> {
-                        for (QueryDocumentSnapshot snapshots : snapshot){
-
-                            GeoPoint geoPoint = snapshots.getGeoPoint("position");
-
-                            double lat = geoPoint.getLatitude();
-                            double lng = geoPoint.getLongitude();
-
-                            LatLng latLng = new LatLng(lat, lng);
-                            googleMap.addMarker(new MarkerOptions().position(latLng).title(placeTitle));
-
-                            // Camera position
-                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
-                            // Zoom animation
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13.0f), 900, null);
-
-                        }
-                    });
-        }
-    };
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        // Get bundle values
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            emirateId = bundle.getString("emirateId");
+            placeId = bundle.getString("placeId");
+            placeTitle = bundle.getString("placeName");
+
+        }
+
+        // Initialize document reference
+        detailsRef = fStore.collection("emirates")
+                .document(emirateId)
+                .collection("places")
+                .document(placeId)
+                .collection("details");
 
         // Toolbar
         Toolbar toolbar = view.findViewById(R.id.toolbar);
@@ -122,12 +111,176 @@ public class MapsFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        detailsRef.get()
+                .addOnSuccessListener(snapshot -> {
+                    for (QueryDocumentSnapshot snapshots : snapshot) {
+
+                        GeoPoint geoPoint = snapshots.getGeoPoint("position");
+
+                        double lat = geoPoint.getLatitude();
+                        double lng = geoPoint.getLongitude();
+
+                        LatLng latLng = new LatLng(lat, lng);
+                        googleMap.addMarker(new MarkerOptions().position(latLng).title(placeTitle));
+
+                        // Camera position
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                        // Zoom animation
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13.0f), 900, null);
+
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+
+                            return;
+                        }
+
+
+                        googleMap.setMyLocationEnabled(true);
+
+                    }
+                });
+
+    }
+
+
+    // Check permissions
+    private boolean checkMapServices(){
+        if(isServicesOK()){
+            if(isMapsEnabled()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public boolean isMapsEnabled(){
+        final LocationManager manager = (LocationManager) getActivity().getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+
+            // Alert Dialog
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("The map requires permission to work properly, do you want to enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                            // Redirect to the application's settings
+                            Intent intent = new Intent();
+                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+                            intent.setData(uri);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Toast.makeText(getActivity(),"Grant permission in settings to access map.", Toast.LENGTH_SHORT).show();
+
+                            // Redirect to home page
+                            startActivity(new Intent(getActivity(), MainActivity.class));
+                        }
+                    });
+            final AlertDialog alert = builder.create();
+            alert.show();
         }
     }
+
+    public boolean isServicesOK(){
+        Log.d("TAG", "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getActivity());
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d("TAG", "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d("TAG", "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(getActivity(), available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(getActivity(), "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("TAG", "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if(!locationPermissionGranted){
+                    getLocationPermission();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(checkMapServices()) {
+            if (!locationPermissionGranted){
+                getLocationPermission();
+            }
+        }
+    }
+
 }
